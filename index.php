f9641242599f3b78b3432b3625017ede563c9a7e
99009744 <55435447+99009744@users.noreply.github.com>
2020-03-17 12:34:26 +0100

    10 characters page begonnen
    afbeeldingen toegevoegd, en pagina opgemaakt

---
+++ index.php
@@ -0,0 +1,54 @@
+<?PHP
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lacquer&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet">
+    <title>Home page</title>
+</head>
+<body>
+    <div id="header">
+        <h1 id="headertext">Alle 10 Characters uit de database</h1>
+    </div>
+    <div id="characters">
+        <div class="charactersprofiles">
+            <img src="images/bowser.jpg" id="bowserimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/buffy.jpg" id="buffyimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/buzz.jpg" id="buzzimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/captianamerica.jpg" id="captianamerica">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/catwoman.jpg" id="catwomanimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/conan.jpg" id="conanimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/quinn.jpg" id="quinnimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/mario.jpg" id="marioimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/shrek.jpg" id="shrekimage">
+        </div>
+        <div class="charactersprofiles">
+            <img src="images/zelda.jpg" id="zeldaimage">
+        </div>
+    </div>
+    <div id="footer">
+        <h3 id="footertext">© Mitchell Kneefel 2020</h3>
+    </div>
+</body>
+</html>
